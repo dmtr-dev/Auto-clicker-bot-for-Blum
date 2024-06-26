c3a95f49bddc4e2acafa0d299c921d799724c4b7
dmtr <samusevk74@gmail.com>
2024-06-26 14:28:13 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Auto clicker bot for Blum
+
+# TG Channel Creator- https://t.me/dmtrcrypto [![Join our Telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/dmtrcrypto)
+
+# РУССКИЙ
+
+### Этот бот используется для автоматического нажатия на зеленые звездочки в игре Blum в однопоточном режиме в указанном окне.
+
+## Установка
+1. **Python 3.11**
+2. **Загружает программу с Github или с помощью консоли на компьютере:**
+ 
+    ```
+    git clone https://github.com/dmtr-dev/Auto-clicker-bot-for-Blum.git
+    ```
+3. **Устанавливает библиотеки в той же консоли:**
+
+    ```
+    pip install pynput loguru pyautogui pygetwindow keyboard
+    ```
+4.  **Запускаем файл `main.py` двойным нажатием**
+
+# Детали
+- Если окно с игрой Blum называется "TelegramDesktop", то просто нажмите Y\y.
+- Для остановки бота, нажмите на кнопку "q".
+
+# Контакт
+# TG - https://t.me/dmtrcrypto [![Join our Telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/dmtrcrypto)
+
+
+# ENGLISH
+
+### This bot is used to automatically click on the green stars in the Blum game in single-threaded mode in the specified window.
+
+## Installation
+1. **Python 3.11**
+2. **Downloads the program from Github or using the console on the computer:**
+ 
+    ```
+    git clone https://github.com/dmtr-dev/Auto-clicker-bot-for-Blum.git
+    ```
+3. **Installs Libraries in the same console:**
+
+    ```
+    pip install pynput loguru pyautogui pygetwindow keyboard
+    ```
+4.  **Run the file `main.py` double tap**
+
+# Details
+- If the Blum game window is called "TelegramDesktop", then just click Y\y.
+- To stop the bot, click on the "q" button.
+
+# Contact
+# TG - https://t.me/dmtrcrypto [![Join our Telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/dmtrcrypto)
---
+++ main.py
@@ -0,0 +1,89 @@
+from pynput.mouse import Button, Controller
+from ctypes import windll
+from loguru import logger
+from sys import stderr
+import pyautogui as pag
+import pygetwindow as gw
+import keyboard
+import random
+import time
+
+mouse_controller = Controller()
+
+pixel_condition = lambda r, g, b: (r in range(100, 220)) and (g in range(200, 255)) and (b in range(0, 125))
+
+windll.kernel32.SetConsoleTitleW('Auto clicker bot for Blum | by https://t.me/dmtrcrypto')
+
+logger.remove()
+logger.add(stderr, format='<cyan>{time:HH:mm:ss}</cyan> | <level>{level:<8}</level> | <cyan><bold>{line}</bold></cyan> | <magenta>Message:</magenta> <level><underline>{message}</underline></level>')
+
+print("\n\n\033[94mTG Channel Creator - https://t.me/dmtrcrypto\033[0m\n")
+
+def click(x, y):
+    mouse_controller.position = (x, y + random.randint(1, 3))
+    mouse_controller.press(Button.left)
+    mouse_controller.release(Button.left)
+
+input_window_name = input('\nEnter the name of the window with Blum game enabled (Y/y - TelegramDesktop): ')
+print("\n")
+
+if input_window_name.lower() == 'y':
+    window_name = "TelegramDesktop"
+else:
+    window_name = input_window_name
+
+
+check_window = gw.getWindowsWithTitle(window_name)
+if not check_window:
+    logger.warning(f'The window "{window_name}" was not found')
+else:
+    logger.success(f'The window "{window_name}" was found successfully')
+    print("\n")
+    logger.info("Use the 'q' button to pause")
+
+telegram_window = check_window[0] if check_window else None
+paused = False
+
+while True:
+    if keyboard.is_pressed('q'):
+        paused = not paused
+        if paused:
+            logger.info('Mode - Stopped')
+        else:
+            logger.info('Mode - Work')
+        time.sleep(0.25)
+
+    if paused:
+        continue
+
+    if not telegram_window or not telegram_window.visible:
+        logger.error(f'Window - "{window_name}" closed or not found')
+        logger.error("Press Enter to Exit...")
+        input()
+        break
+
+    win_rect  = (telegram_window.left, telegram_window.top, telegram_window.width, telegram_window.height)
+
+    try:
+        telegram_window.activate()
+    except:
+        telegram_window.minimize()
+        telegram_window.restore()
+
+    screenshot = pag.screenshot(region=win_rect)
+    width, height = screenshot.size
+
+    pixel_detected = False
+
+    for x in range(0, width, 20):
+        for y in range(0, height, 20):
+            r, g, b = screenshot.getpixel((x, y))
+            if pixel_condition(r, g, b):
+                click_x = win_rect[0] + x
+                click_y  = win_rect[1] + y
+                click(click_x + 4, click_y)
+                time.sleep(0.001)
+                pixel_found = True
+                break
+        if pixel_detected:
+            break
