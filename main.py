62f09a84ace2683491cc33b6f5ebd2950b71c3ae
dmtr <samusevk74@gmail.com>
2024-07-01 17:55:15 +0300

    update

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import time
 
 mouse_controller = Controller()
 
-pixel_condition = lambda r, g, b: (r in range(105, 200)) and (g in range(200, 255)) and (b in range(0, 120))
+def pixel_condition(r, g, b):
+    return (r in range(105, 200) and g in range(200, 255) and b in range(0, 120))
 
 windll.kernel32.SetConsoleTitleW('Auto clicker bot for Blum | by https://t.me/dmtrcrypto')
 
@@ -20,11 +21,13 @@ logger.add(stderr, format='<cyan>{time:HH:mm:ss}</cyan> | <level>{level:<8}</lev
 print("\n\n\033[94mTG Channel Creator - https://t.me/dmtrcrypto\033[0m\n")
 
 def click(x, y):
-    mouse_controller.position = (x, y + random.randint(1, 3))
+    mouse_controller.position = (x, y)
     mouse_controller.press(Button.left)
     mouse_controller.release(Button.left)
 
-input_window_name = input('\nEnter the name of the window with Blum game enabled (Y/y - TelegramDesktop): ')
+input_window_name = input('\nEnter the name of the window with Blum game enabled (Y\y - TelegramDesktop): ')
+print("\n")
+input_button = input("Do you want the bot to play continuously without your participation until you run out of tickets (Y/n): ")
 print("\n")
 
 if input_window_name.lower() == 'y':
@@ -37,11 +40,11 @@ check_window = gw.getWindowsWithTitle(window_name)
 if not check_window:
     logger.warning(f'The window "{window_name}" was not found')
 else:
+    telegram_window = check_window[0]
     logger.success(f'The window "{window_name}" was found successfully')
     print("\n")
     logger.info("Use the 'q' button to pause")
 
-telegram_window = check_window[0] if check_window else None
 paused = False
 
 while True:
@@ -74,16 +77,35 @@ while True:
     width, height = screenshot.size
 
     pixel_detected = False
+    button_detected = False
 
-    for x in range(0, width, 20):
-        for y in range(0, height, 20):
+    if pixel_detected:
+        break    
+
+    for x in range(0, width, 10):
+        for y in range(0, height, 10):
             r, g, b = screenshot.getpixel((x, y))
             if pixel_condition(r, g, b):
                 click_x = win_rect[0] + x
-                click_y  = win_rect[1] + y
-                click(click_x + 4, click_y)
-                time.sleep(0.01)
-                pixel_found = True
+                click_y = win_rect[1] + y
+                click(click_x + random.uniform(1, 2), click_y + random.uniform(1, 2))
+                time.sleep(0.001)
+                pixel_detected = True
                 break
-        if pixel_detected:
+            
+            if (y >= height - 100 and x >= width - 50) and (r, g, b) == (255, 255, 255) and input_button.lower() == 'y':
+                time.sleep(random.uniform(1, 2))
+                click_x = win_rect[0] + x
+                click_y = win_rect[1] + y
+                click(click_x + random.uniform(1, 2), click_y + random.uniform(1, 2))
+                button_detected = True
+                break
+            if (y >= height - 100 and x >= width - 50) and (r, g, b) == (40, 40, 40):
+                print("\n")
+                logger.info("Tickets are out.")
+                print("\n")
+                logger.info("Press Enter to Exit...")
+                input()
+                break
+        if button_detected:
             break
