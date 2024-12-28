7ce37448b75004f3eba93b8efaabf3572fc2bb3b
dmtr <93618083+dmtr-dev@users.noreply.github.com>
2024-12-28 14:49:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,25 +9,18 @@ import keyboard
 import random
 import time
 
-
 mouse_controller = Controller()
 
-# def pixel_condition(r, g, b):
-#     return ((r in range(220, 230) and g in range(115, 130) and b in range(45, 55)) or        # color Pumpkin
-#             (r in range(125, 131) and g in range(215, 222) and b in range(229, 233)) or      # color Freezing
-#             (r in range(134, 141) and g in range(126, 132) and b in range(126, 132)) or      # color Bomb
-#             (r in range(178, 206) and g in range(178, 206) and b in range(178, 206)))        # color Bomb 2
-
 def pixel_condition(r, g, b):
-    return ((r in range(90, 110) and g in range(125, 150) and b in range(85, 95)) or        #tree
-            (r in range(200, 255) and g in range(35, 65) and b in range(180, 205)) or       #boot
-            (r in range(50, 105) and g in range(110, 175) and b in range(5, 40)) or         #ball
-            (r in range(185, 255) and g in range(0, 2) and b in range(155, 200)) or         #wand
-            (r in range(90, 130) and g in range(165, 210) and b in range(20, 85)) or        #berries
-            (r in range(130, 180) and g in range(55, 75) and b in range(5, 20)) or          #gingerbread
-            (r in range(190, 240) and g in range(10, 35) and b in range(100, 175)) or       #head
-            (r in range(235, 255) and g in range(150, 185) and b in range(0, 15)) or        #candle
-            (r in range(85, 105) and g in range(135, 175) and b in range(15, 55)))          #decoration
+    return ((r in range(200, 255) and g in range(34, 65) and b in range(184, 200)) or         # candy
+            (r in range(87, 129) and g in range(160, 203) and b in range(23, 60)) or          # berries
+            (r in range(209, 239) and g in range(0, 14) and b in range(149, 167)) or          # head
+            (r in range(234, 234) and g in range(250, 251) and b in range(120, 122)) or       # head
+            (r in range(132, 180) and g in range(56, 88) and b in range(7, 38)) or            # gingerbread
+            (r in range(43, 58) and g in range(88, 111) and b in range(24, 36)) or            # tree
+            (r in range(176, 183) and g in range(236, 242) and b in range(250, 255)) or       # candle
+            (r in range(196, 255) and g in range(15, 32) and b in range(172, 195)))           # boots
+
 
 windll.kernel32.SetConsoleTitleW('Auto clicker bot for Blum | by https://t.me/dmtrcrypto')
 cprint("\nTG Channel - https://t.me/dmtrcrypto\n\n", 'magenta')
@@ -84,19 +77,18 @@ while True:
     screenshot = pyautogui.screenshot(region=win_rect)
     width, height = screenshot.size
 
-    for x in range(0, width, 20): # parameters for slowing down the bot
-        for y in range(0, height, 20): # parameters for slowing down the bot
+    for x in range(0, width, 25): # parameters for slowing down the bot
+        for y in range(0, height, 25): # parameters for slowing down the bot
             r, g, b = screenshot.getpixel((x, y))
             if pixel_condition(r, g, b):
                 click_x = win_rect[0] + x
                 click_y = win_rect[1] + y
                 click(click_x + random.uniform(1, 2), click_y + random.uniform(1, 2))
-                time.sleep(0.01)
+                time.sleep(0.02)
                 break
 
     if time.time() - last_check_time > 8 and input_button.lower() == 'y':
         last_check_time = time.time()
-        click(win_rect[0] + random.randint(398, 402), win_rect[1] + random.randint(498, 502))
         click(win_rect[0] + random.randint(348, 352), win_rect[1] + random.randint(608, 612))
         time.sleep(0.1)
 
